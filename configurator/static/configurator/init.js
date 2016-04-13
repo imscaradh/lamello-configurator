d5f3493b74cba3f030506edcb839f03b18316d07
Pascal Zingg <pzingg@fastforward.ch>
2016-04-14 00:12:40 +0200

    added jquery hover functionality to dropdown

--- configurator/static/configurator/init.js
+++ configurator/static/configurator/init.js
@@ -1,3 +1,19 @@
 $(function () {
+    $('.connection a').hover(function(e) {
+	var $target = $(e.target);
+	console.info("hovered " + $target.text());
+	$(".preview").html($target.text());
+    });
 
+    $('.connection a').click(function(e) {
+	$('.connection .selected-text').html($(e.target).text());
+    });
+
+    function isConnectionPossible(m1, m2, angle) {
+	console.info("Material 1: " + m1);
+	console.info("Material 2: " + m2);
+	console.info("Angle: " + angle);
+
+	//TODO: Calculation
+    }
 });
