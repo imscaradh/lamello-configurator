330796d79d56ca7324ce84654170e2c990d7a8ea
Pascal Zingg <pzingg@fastforward.ch>
2016-05-01 18:32:30 +0200

    abstraction of services and implemented zeta calculation

--- configurator/services.py
+++ configurator/services.py
@@ -1,9 +1,43 @@
 import math
 
 
-class BisecService:
+class Service:
+    m1_width = 0
+    m2_width = 0
+    angle = 90
+    gehrung = 0
+    links = []
+    rechts = []
+    results = {}
 
-    links = 5.9
+    def __init__(self, m1_width, m2_width, angle):
+        self.m1_width = m1_width
+        self.m2_width = m2_width
+        self.angle = angle
+
+        self.gehrung = abs(90 - angle / 2)
+
+        self.results['cnc'] = {}
+        self.results['cnc']['possible'] = False
+        self.results['zeta'] = {}
+        self.results['zeta']['0mm'] = False
+        self.results['zeta']['2mm'] = False
+        self.results['zeta']['4mm'] = False
+
+    def zeta_0mm(self):
+        raise NotImplementedError("Please Implement this method")
+
+    def zeta_2mm(self):
+        raise NotImplementedError("Please Implement this method")
+
+    def zeta_4mm(self):
+        raise NotImplementedError("Please Implement this method")
+
+    def check(self):
+        raise NotImplementedError("Please Implement this method")
+
+
+class BisecService(Service):
 
     # Parameters for P-10
     p1 = 8.46
@@ -11,14 +45,9 @@ class BisecService:
     p3 = 10
     p4 = 2.7
 
-    m1_width = 0
-    m2_width = 0
-    angle = 90
-
     def __init__(self, m1_width, m2_width, angle):
-        self.m1_width = m1_width
-        self.m2_width = m2_width
-        self.angle = angle
+        Service.__init__(self, m1_width, m2_width, angle)
+        self.links.append(5.9)
 
     def calc_h(self):
         kontaktdistanz = math.sqrt(((self.m2_width + self.m1_width / math.cos(self.angle / 180 * math.pi))
@@ -41,39 +70,51 @@ class BisecService:
                        / math.tan(schnittwinkel / 180 * math.pi) - self.p4)
 
         rechts = rechts_niedrig if rechts_niedrig < rechts_hoch else rechts_hoch
-        space_range = rechts - BisecService.links
-        position = (BisecService.links + rechts) / 2
+        # FIXME: use right links!!
+        space_range = rechts - self.links[0]
+        position = (self.links[0] + rechts) / 2
 
         return {'rechts': rechts, 'range': space_range, 'position': position}
 
+    def zeta_0mm(self):
+        cond1 = (self.m1_width == self.m2_width)
+        cond2 = (max(self.links) < 14 - (math.tan(self.gehrung / 180 * math.pi) * 4 / math.sin(self.gehrung / 180 * math.pi)
+                 - math.tan(self.gehrung / 180 * math.pi) * 4))
+        cond3 = ((14 - (math.tan(self.gehrung / 180 * math.pi) * 4 / math.sin(self.gehrung / 180 * math.pi) -
+                 math.tan(self.gehrung / 180 * math.pi) * 4)) < min(self.rechts))
+
+        return cond1 and cond2 and cond3
+
+    def zeta_2mm(self):
+        cond1 = (self.m1_width == self.m2_width)
+        cond2 = (max(self.links) < 14 - ((math.tan(self.gehrung / 180 * math.pi) * 4 / math.sin(self.gehrung / 180 * math.pi)
+                 - math.tan(self.gehrung / 180 * math.pi) * 4) + 2 / math.cos(self.gehrung / 180 * math.pi)))
+        cond3 = (14 - ((math.tan(self.gehrung / 180 * math.pi) * 4 / math.sin(self.gehrung / 180 * math.pi) -
+                 math.tan(self.gehrung / 180 * math.pi) * 4) + 2 / math.cos(self.gehrung / 180 * math.pi)) < min(self.rechts))
+
+        return cond1 and cond2 and cond3
+
+    def zeta_4mm(self):
+        return self.zeta_2mm()
+
     def check(self):
         horizontal = self.calc_h()
+        self.rechts.append(horizontal['rechts'])
         vertical = self.calc_v()
+        self.rechts.append(vertical['rechts'])
 
         range_h = horizontal['range']
         range_v = vertical['range']
 
-        results = {}
-        results['cnc'] = {}
-        results['zeta'] = {}
-
         if range_h > 0 and range_v > 0:
-            results['cnc']['possible'] = True
+            self.results['cnc']['possible'] = True
             rechts_h = horizontal['rechts']
             rechts_v = vertical['rechts']
             # FIXME: What is links?
-            results['cnc']['position'] = (max(self.links, self.links) + min(rechts_v, rechts_h)) / 2
-        else:
-            results['cnc']['possible'] = False
+            self.results['cnc']['position'] = (max(self.links[0], self.links[0]) + min(rechts_v, rechts_h)) / 2
 
-        # TODO: Calculate
-        if True:
-            results['zeta']['0mm'] = True
-            results['zeta']['2mm'] = True
-            results['zeta']['4mm'] = True
-        else:
-            results['zeta']['0mm'] = False
-            results['zeta']['2mm'] = False
-            results['zeta']['4mm'] = False
+        self.results['zeta']['0mm'] = self.zeta_0mm()
+        self.results['zeta']['2mm'] = self.zeta_2mm()
+        self.results['zeta']['4mm'] = self.zeta_4mm()
 
-        return results
+        return self.results
