14f83120f1e06069de6564a32c8f2feab924c5b5
Pascal Zingg <pzingg@fastforward.ch>
2016-05-02 22:17:29 +0200

    fixed tests

--- configurator/tests.py
+++ configurator/tests.py
@@ -1,13 +1,18 @@
 from django.test import TestCase
 from .services import BisecService
+from .models import Connector
 import math
 
 
 class ServicesTestCase(TestCase):
 
     def setUp(self):
-        # nothing here
-        return
+        p1 = Connector(name="P10", p1=8.46, p2=4.9, p3=10, p4=2.7)
+        p1.save()
+        p2 = Connector(name="P14", p1=8.46, p2=4.9, p3=10, p4=2.7)
+        p2.save()
+        p3 = Connector(name="P1014", p1=8.46, p2=4.9, p3=10, p4=2.7)
+        p3.save()
 
     def test_calc_bisec(self):
         m1 = 20
@@ -15,6 +20,7 @@ class ServicesTestCase(TestCase):
         angle = 90
 
         bisec = BisecService(m1, m2, angle)
+        bisec.set_connector("P10")
         result = bisec.check()
         print(result)
         is_close = math.isclose(result['cnc']['position'], 7.87, rel_tol=1e-3)
