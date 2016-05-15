287aca2a8c8f595499264db90c05ac1f7e5419ac
Pascal Zingg <pazingg@gmail.com>
2016-05-15 15:25:06 +0200

    implemented unit switcher

--- configurator/views.py
+++ configurator/views.py
@@ -59,18 +59,27 @@ def calc(request):
     error_msg = HttpResponse(status=500)
 
     if request.method == 'POST' and request.POST is not None:
+
+        angle = request.POST['angle']
         m1_width = request.POST['m1']
         m2_width = request.POST['m2']
-        angle = request.POST['angle']
+        unit = request.POST['unit']
+
         connection_type = request.POST['connection_type']
 
-        if None in (m1_width, m2_width, angle, connection_type):
+        if None in (unit, m1_width, m2_width, angle, connection_type):
             return error_msg
 
         try:
+            angle = float(angle)
             m1_width = float(m1_width)
             m2_width = float(m2_width)
-            angle = float(angle)
+            if unit == "in":
+                m1_width = m1_width * 25.4
+                m2_width = m2_width * 25.4
+
+            import pdb
+            pdb.set_trace()
 
             calc_results = {}
             service = ConnectorService.factory(connection_type, m1_width, m2_width, angle)
