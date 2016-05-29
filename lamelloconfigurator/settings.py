b41126e9412fe5330f55ed257d8d8fc9abb2f575
Pascal Zingg <pazingg@gmail.com>
2016-05-29 15:43:31 +0200

    configured logging

--- lamelloconfigurator/settings.py
+++ lamelloconfigurator/settings.py
@@ -28,6 +28,30 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+        },
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': './debug.log',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'file'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+    },
+}
+
 
 # Application definition
 
