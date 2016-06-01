137fce3671da5fc1f5693dd9df53c625217eead7
Pascal Zingg <pazingg@gmail.com>
2016-06-01 14:04:51 +0200

    admin tool updated doc

--- configurator/admin.py
+++ configurator/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from configurator.models import ConnectionType, Connector
 
 
+# This allows to edit the model data
 admin.site.register(ConnectionType)
 admin.site.register(Connector)
